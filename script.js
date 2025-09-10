35c7d5810bf365dbf41bf1b7dfb5b702030a488a
Jennifer <jennifer.rattana2003@gmail.com>
2025-09-10 14:30:17 -0700

    Created a new File for Javascripting
    Experimenting...

---
+++ script.js
@@ -0,0 +1,9 @@
+/* The initial plan for this is to find a way to click on the vault image in index.html and 
+have it transform into the open vault image for some interactivity on the home page.
+*/
+
+<script>
+  // Trying to use JavaScript to swap images when clicked
+  
+  
+</script>
