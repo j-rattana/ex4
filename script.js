37b771e9f048baefe11f27900671a87e60f43080
Jennifer <jennifer.rattana2003@gmail.com>
2025-09-10 15:07:55 -0700

    Attempting Click Function for Vault
    Lets gooooo experimenting with two images, open and closed.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /* The initial plan for this is to find a way to click on the vault image in index.html and 
-have it transform into the open vault image for some interactivity on the home page.
+have it transform into the open vault image for some interactivity on the home page. I am having
+ChatGPT help me understand how to do certain codes and what each part means.
 */
 
-<script>
-  // Trying to use JavaScript to swap images when clicked
-  
-  
-</script>
+  // Trying to use JavaScript to swap images when clicked (Credits to ChatGPT)
+  var vault=document.getElementById("ClosedVault");
+
+vault.addEventListener("click", function() {
+  if (vault.src.includes("metallic-bank-vault-door-isolated-white.png")) {
+    vault.src="./assets/half-open-bank-vault-door-white-safe-bank-metal-door-safe-lock-security-bank-open-safe-bank-vector-illustration.png";
+  } else {
+    vault.src="./assets/metallic-bank-vault-door-isolated-white.png";
+  }
+});
